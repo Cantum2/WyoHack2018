0e2bb4e4d58fe2423dd2749da2da3a005fdab198
Marcus <cantumarcus19@gmail.com>
2018-09-08 14:27:39 -0600

    Worked on the styles for discover view

--- App/app.routes.js
+++ App/app.routes.js
@@ -6,6 +6,26 @@ app.config(['$routeProvider', function($routeProvider){
     templateUrl: 'App/Views/Landing.html',
     controller: 'LandingCtrl'
   })
+  .when('/discover', {
+    templateUrl: 'App/Views/Discover.html',
+    controller: 'DiscoverCtrl'
+  })
+  .when('/documents', {
+    templateUrl: 'App/Views/Documents.html',
+    controller: 'DocumentsCtrl'
+  })
+  .when('/team', {
+    templateUrl: 'App/Views/Team.html',
+    controller: 'TeamCtrl'
+  })
+  .when('/roadmap', {
+    templateUrl: 'App/Views/Roadmap.html',
+    controller: 'RoadmapCtrl'
+  })
+  .when('/faq', {
+    templateUrl: 'App/Views/Faq.html',
+    controller: 'FaqCtrl'
+  })
   .otherwise({
     redirectTo: '/landing'
   });
